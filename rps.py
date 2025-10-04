9f8b0cbead13cb31c99f2b967c2ae1fcee5ad56c
NYCoaster <159592422+NYCoaster@users.noreply.github.com>
2025-10-04 19:55:16 +0000

    rps finished

---
+++ rps.py
@@ -0,0 +1,21 @@
+import random
+playerselection = input ('Please Pick a Number. Rock is 0, Paper is 1, and Scissors is 2')
+botselection = random.randint(0,2)
+if botselection == 0 and playerselection == '2':
+    print ('You lost bot picked rock')
+elif botselection == 0 and playerselection == '1':
+    print ('You won bot picked rock')
+elif botselection == 0 and playerselection == '0':
+    print ('You tied bot picked rock')
+elif botselection == 1 and playerselection == '0':
+    print ('You lost bot picked paper')
+elif botselection == 1 and playerselection == '2':
+    print ('You won bot picked paper')
+elif botselection == 1 and playerselection == '1':
+    print ('You tied bot picked paper')
+elif botselection == 2 and playerselection == '1':
+    print ('You lost bot picked scissors')
+elif botselection == 2 and playerselection == '0':
+    print ('You won bot picked scissors')
+elif botselection == 2 and playerselection == '1':
+    print ('You tied bot picked scissors')
