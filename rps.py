200753ac9df51f8702e7751e61411e324b2dcdb8
NYCoaster <159592422+NYCoaster@users.noreply.github.com>
2025-10-04 20:56:54 +0000

    added play again

--- rps.py
+++ rps.py
@@ -19,3 +19,37 @@ elif botselection == 2 and playerselection == '0':
     print ('You won bot picked scissors')
 elif botselection == 2 and playerselection == '1':
     print ('You tied bot picked scissors')
+again = input ('Do you want to play again')
+
+while again := 'y':
+    import random
+    playerselection = input ('Please Pick a Number. Rock is 0, Paper is 1, and Scissors is 2')
+    botselection = random.randint(0,2)
+    if botselection == 0 and playerselection == '2':
+        print ('You lost bot picked rock')
+        again = input ('Do you want to play again')
+    elif botselection == 0 and playerselection == '1':
+        print ('You won bot picked rock')
+        again = input ('Do you want to play again')
+    elif botselection == 0 and playerselection == '0':
+        print ('You tied bot picked rock')
+        again = input ('Do you want to play again')
+    elif botselection == 1 and playerselection == '0':
+        print ('You lost bot picked paper')
+        again = input ('Do you want to play again')
+    elif botselection == 1 and playerselection == '2':
+        print ('You won bot picked paper')
+        again = input ('Do you want to play again')
+    elif botselection == 1 and playerselection == '1':
+        print ('You tied bot picked paper')
+        again = input ('Do you want to play again')
+    elif botselection == 2 and playerselection == '1':
+        print ('You lost bot picked scissors')
+        again = input ('Do you want to play again')
+    elif botselection == 2 and playerselection == '0':
+        print ('You won bot picked scissors')
+        again = input ('Do you want to play again')
+    elif botselection == 2 and playerselection == '1':
+        print ('You tied bot picked scissors')
+        again = input ('Do you want to play again')
+
